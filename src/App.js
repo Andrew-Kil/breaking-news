c2dec78c0d56b93b7bfc581d8ce88f3c8ec9f2a8
Andrew KIl <kil.andrew@gmail.com>
2020-08-11 12:41:52 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Feed from "./components/Feed/Feed";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+import requests from "./helpers/requests";
 
 function App() {
-  return <div className="App">App</div>;
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getNews = async () => {
+    const news = await fetch(requests.topHeadlinesUS)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        return data.articles;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return news;
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    try {
+      const fetchNews = async () => {
+        const news = await getNews();
+        setNews(news);
+      };
+      fetchNews();
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message);
+      setIsLoading(false);
+    }
+  }, []);
+
+  console.log("news", news);
+
+  return (
+    <>
+      <Header />
+      {error ? <p>{error}</p> : <Feed news={news} isLoading={isLoading} />}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
