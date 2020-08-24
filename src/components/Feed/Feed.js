edf901820dc3fb652d32789db9cd565f099bece5
Andrew KIl <kil.andrew@gmail.com>
2020-08-23 20:55:57 -0400

    update Feed component

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,41 +1,19 @@
 import React from "react";
 
-import placeholder from "../../assets/news-placeholder.png";
+import Article from "../Article/Article";
 
 import "./Feed.css";
 
 function Feed({ news, isLoading }) {
   return (
-    <>
+    <div className="feed-container">
       {isLoading && <p>Loading...</p>}
       {news &&
         !isLoading &&
         news.map((article, idx) => {
-          const {
-            urlToImage,
-            title,
-            source,
-            description,
-            content,
-            publishedAt,
-            url,
-          } = article;
-          return (
-            <div key={idx}>
-              <img
-                src={urlToImage ? urlToImage : placeholder}
-                alt={title}
-                className="article-img"></img>
-              <h3>{title.split("-")[0].slice(0, -1)}</h3>
-              <h6>{source ? source.name : ""}</h6>
-              <p>{description}</p>
-              <p>{content}</p>
-              <p>Published: {new Date(publishedAt).toDateString()}</p>
-              <a href={url}>{url}</a>
-            </div>
-          );
+          return <Article idx={idx} article={article} />;
         })}
-    </>
+    </div>
   );
 }
 
